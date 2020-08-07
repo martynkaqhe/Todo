719aab9c5785884eaeeb2dbaaea3671847d9d764
Adam Parchimowicz <adam.parchimowicz@contractors.roche.com>
2020-08-07 11:24:46 +0200

    Some changes

--- todo.js
+++ todo.js
@@ -1,21 +1,51 @@
-function todolist(){
-    let newQuest = document.getElementById("nowe").value;
+const allTasks = [];
+let numerator = 1;
+
+function todolist() {
+    const allTasks = [];
+    let newTaskName = document.getElementById("nowe").value;
     
-    const allQuest = [];
-    let numeracja = [0];
+    
    
-        if(newQuest == "" ){
-            document.getElementById("lista").innerHTML = "Musisz wpisać zadanie!!!";
-         }
-        else{
-            allQuest.push(newQuest);
-            numeracja++
-            document.getElementById("lista").innerHTML = numeracja + '. ' + allQuest[0];
-            newQuest = document.getElementById("nowe").value;
-            document.getElementById("usuniecie").style.display = "block";
-         }
+    if(newTaskName == "" ) {
+        document.getElementById("message").innerHTML = "Musisz wpisać zadanie!!!";
+        return;
+    }
+
+    document.getElementById("message").innerHTML = "";
+    allTasks.push(newTaskName);
+    
+    const newTaskTextNode = document.createTextNode(newTaskName);
+    const newTaskElement = document.createElement("li");
+    newTaskElement.appendChild(newTaskTextNode);
+    document.getElementById("lista").appendChild(newTaskElement);
+
+    document.getElementById("nowe").value = "";
+}
+
+function addTask() {
+    let newTaskName = document.getElementById("nowe").value;
+    if(newTaskName == "" ) {
+        document.getElementById("message").innerHTML = "Musisz wpisać zadanie!!!";
+        return;
+    }
+
+    let task = {
+        id: numerator++,
+        taskName: newTaskName
+    }
+}
+
+function removeTask(taskId) {
 
 }
+
+function renderTasks() {
+    array.forEach(allTasks => {
+        
+    });
+}
+
 function usun(){
     document.getElementById("lista").innerHTML = '';
     document.getElementById("usuniecie").style.display = "none";
