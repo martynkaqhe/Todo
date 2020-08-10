ad149008c19721e82f6040a6569a4cef08c52b38
Martyna Borkowska <marti.borkow@gmail.com>
2020-08-10 10:19:32 +0200

    remove

--- todo.js
+++ todo.js
@@ -18,11 +18,18 @@ function todolist() {
     const newTaskTextNode = document.createTextNode(newTaskName);
     const newTaskElement = document.createElement("li");
     newTaskElement.appendChild(newTaskTextNode);
+    document.getElementById('usuniecie').style.display = "block";
     document.getElementById("lista").appendChild(newTaskElement);
-
     document.getElementById("nowe").value = "";
+    
 }
 
+function usun(){
+    document.getElementById("lista").innerHTML = '';
+    document.getElementById("usuniecie").style.display = "none";
+}
+ 
+/*
 function addTask() {
     let newTaskName = document.getElementById("nowe").value;
     if(newTaskName == "" ) {
@@ -37,6 +44,7 @@ function addTask() {
 }
 
 function removeTask(taskId) {
+    
 
 }
 
@@ -45,11 +53,7 @@ function renderTasks() {
         
     });
 }
-
-function usun(){
-    document.getElementById("lista").innerHTML = '';
-    document.getElementById("usuniecie").style.display = "none";
-}
+ */
 
 
 
